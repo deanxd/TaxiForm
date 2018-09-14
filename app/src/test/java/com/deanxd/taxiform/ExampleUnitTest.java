6a84228ac356a0df4a269face8113f4a37b7c403
deanxd <deanxd@foxmail.com>
2018-09-15 01:42:02 +0800

    Initial commit

---
+++ app/src/test/java/com/deanxd/taxiform/ExampleUnitTest.java
@@ -0,0 +1,64 @@
+package com.deanxd.taxiform;
+
+import android.support.annotation.NonNull;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testForInterceptor() throws Exception {
+        Interceptor interceptor = new Interceptor() {
+            @Override
+            public Response intercept(@NonNull Chain chain) throws IOException {
+
+                System.out.println("this is one interceptor");
+                Request request = chain.request();
+                return chain.proceed(request);
+            }
+        };
+
+        Interceptor interceptorTwo = new Interceptor() {
+            @Override
+            public Response intercept(@NonNull Chain chain) throws IOException {
+                System.out.println("this is two interceptor");
+                Request request = chain.request();
+                return chain.proceed(request);
+            }
+        };
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(interceptorTwo)
+                .addInterceptor(interceptor)
+                .build();
+
+        Request request = new Request.Builder()
+                .get()
+                .url("https://www.baidu.com")
+                .build();
+
+        Response execute = okHttpClient.newCall(request).execute();
+
+        System.out.print("response:" + execute.body().string());
+    }
+}
