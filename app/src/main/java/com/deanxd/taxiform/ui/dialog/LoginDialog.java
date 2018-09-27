0bf2a999415c845af77e4dddef9c4976ad231fa6
deanxd <deanxd@foxmail.com>
2018-09-28 00:56:24 +0800

    some config, and splash activity

---
+++ app/src/main/java/com/deanxd/taxiform/ui/dialog/LoginDialog.java
@@ -0,0 +1,16 @@
+package com.deanxd.taxiform.ui.dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by think on 2018/9/28.
+ */
+
+public class LoginDialog extends Dialog {
+
+    public LoginDialog(@NonNull Context context) {
+        super(context);
+    }
+}
