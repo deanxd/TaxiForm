386088a764e350c2f0473106ebd784a0e0c32f65
zhuxudong <Cajwm4hRXKD6iqdC>
2018-10-26 18:57:14 +0800

    update info

--- app/src/test/java/com/deanxd/taxiform/ExampleUnitTest.java
+++ app/src/test/java/com/deanxd/taxiform/ExampleUnitTest.java
@@ -5,6 +5,8 @@ import android.support.annotation.NonNull;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
@@ -79,12 +81,65 @@ public class ExampleUnitTest {
 
 
     @Test
-    public void testForQueue(){
+    public void testForQueue() {
 //        System.out.println("this is a test for queue");
 
 //        SortDemo.bubblingSorting();
 
-        SortDemo.insertSorting();
+//        SortDemo.insertSorting();
+
+//        SortDemo.selectSorting();
+
+        bucketSort();
+    }
+
+
+    public void bucketSort() {
+        String sourceStr = "DaFB1234cAz";
+
+        StringBuilder[] buckets = new StringBuilder[3];
+
+        for (int i = 0; i < sourceStr.length(); i++) {
+            StringBuilder bucket = null;
+            char value = sourceStr.charAt(i);
+            if (Character.isLowerCase(value)) {
+                bucket = buckets[0];
+                if (bucket == null) {
+                    bucket = new StringBuilder();
+                    buckets[0] = bucket;
+                }
+            } else if (Character.isDigit(value)) {
+                bucket = buckets[1];
+                if (bucket == null) {
+                    bucket = new StringBuilder();
+                    buckets[1] = bucket;
+                }
+            } else if (Character.isUpperCase(value)) {
+                bucket = buckets[2];
+                if (bucket == null) {
+                    bucket = new StringBuilder();
+                    buckets[2] = bucket;
+                }
+            }
+
+            if (bucket != null) {
+                bucket.append(value);
+            }
+        }
+
+        String result = "";
+        if (buckets[0] != null) {
+            result += buckets[0].toString();
+        }
+        if (buckets[1] != null) {
+            result += buckets[1].toString();
+        }
+        if (buckets[2] != null) {
+            result += buckets[2].toString();
+        }
+
+        System.out.println("排序前：" + sourceStr);
+        System.out.println("排序后: " + result);
     }
 
 
--- app/src/test/java/com/deanxd/taxiform/SortDemo.java
+++ app/src/test/java/com/deanxd/taxiform/SortDemo.java
@@ -1,5 +1,7 @@
 package com.deanxd.taxiform;
 
+import java.util.Arrays;
+
 public class SortDemo {
 
     public static void bubblingSorting() {
@@ -9,6 +11,7 @@ public class SortDemo {
 
         for (int i = 0; i < data.length; i++) {
             boolean isSort = false;
+
             for (int j = 0; j < data.length - i - 1; j++) {
                 if (data[j] > data[j + 1]) {
                     int temp = data[j];
@@ -32,17 +35,57 @@ public class SortDemo {
         System.out.println(printArray(data));
 
         for (int i = 1; i < data.length; i++) {
-            for (int j = 0; j < i; j++) {
-                if(data[i] < data[j]){
-                    int temp = data[i];
-                    data[i] = data[j];
-                    data[j] = temp;
+            int value = data[i];
+            int j = i - 1;
+            for (; j >= 0; j--) {
+                if (data[j] > value) {
+                    data[j + 1] = data[j];
+                } else {
+                    break;
                 }
+            }
+            data[j + 1] = value;
+        }
+
+        System.out.println(printArray(data));
+    }
+
+    public static void selectSorting() {
+        int[] data = new int[]{1, 2, 7, 6, 5, 7, 8, 9, 4, 9};
+
+        System.out.println(printArray(data));
+
+        for (int i = 0; i < data.length - 1; i++) {
+            int k = i;
+            for (int j = i + 1; j < data.length; j++) {
+                if (data[j] < data[k]) {
+                    k = j;
+                }
+            }
+
+            if (k != i) {
+                int temp = data[i];
+                data[i] = data[k];
+                data[k] = temp;
             }
         }
 
         System.out.println(printArray(data));
     }
+
+
+    public static void mergeSoft() {
+        int[] arraySum = new int[10];
+        int[] arrayOne = new int[5];
+        int[] arrayTwo = new int[5];
+
+//        arraySum = Arrays.binarySearch();
+
+//        Arrays.
+
+
+    }
+
 
     private static String printArray(int[] array) {
         StringBuilder builder = new StringBuilder();
